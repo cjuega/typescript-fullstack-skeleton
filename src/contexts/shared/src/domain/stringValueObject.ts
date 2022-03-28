122107e4decaa92bc8a1cedb9046935e31a47960
Carlos Juega <juancarlos.juega@optivamedia.com>
2022-03-28 19:17:34 +0200

    do not allow empty strings as StringValueObjects

--- src/contexts/shared/src/domain/stringValueObject.ts
+++ src/contexts/shared/src/domain/stringValueObject.ts
@@ -1,3 +1,18 @@
 import ValueObject from '@src/domain/valueObject';
+import InvalidArgument from '@src/domain/invalidArgument';
 
-export default abstract class StringValueObject extends ValueObject<string> {}
+export default abstract class StringValueObject extends ValueObject<string> {
+    constructor(value: string) {
+        const trimmed = value.trim();
+
+        StringValueObject.ensureStringIsNotEmpty(trimmed);
+
+        super(trimmed);
+    }
+
+    private static ensureStringIsNotEmpty(value: string): void {
+        if (!value) {
+            throw new InvalidArgument(`<${this.name}> doesn't allow empty strings`);
+        }
+    }
+}
