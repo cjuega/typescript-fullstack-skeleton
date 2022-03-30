255668a11bf445903ef4d414faa1b746fbc1b129
Carlos Juega <juancarlos.juega@optivamedia.com>
2022-03-30 14:06:59 +0200

    add script to publish documentation to an S3 bucket

---
+++ scripts/publish-docs-aws.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo "Environment argument not given!"
+    exit 1
+fi
+
+ENV=$1
+
+if [ ! -f .env.$ENV ]; then
+    echo "dotenv file <.env.$ENV> doesn't exist!"
+    exit 1
+fi
+
+source .env.$ENV
+
+if [ -z "$AWS_PROFILE" ]; then
+    AWS_PROFILE=default
+fi
+
+SERVICE_FLAVOR=$(basename $(pwd))
+DOCS=dist/docs/api/openapi.yml
+
+if [ ! -z "$DOCS_S3_BUCKET" ] && [ -f $DOCS ]; then
+    echo "Deploying docs for <$PROJECT ($SERVICE_FLAVOR)>!"
+    AWS_PROFILE=$AWS_PROFILE aws s3 cp $DOCS s3://$DOCS_S3_BUCKET/apis/$PROJECT/$SERVICE_FLAVOR/openapi.yml --cache-control no-cache
+else
+    echo "docs for <$PROJECT ($SERVICE_FLAVOR)> not found or DOCS_S3_BUCKET not found in <.env.$ENV>. Skipping docs deployment..."
+fi
