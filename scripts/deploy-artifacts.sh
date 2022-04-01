5073fde85f449109006f7835c44d06345dcb443d
Carlos Juega <juancarlos.juega@optivamedia.com>
2022-04-01 18:29:43 +0200

    deploy-artifacts script and buildspec typos fixed

--- scripts/deploy-artifacts.sh
+++ scripts/deploy-artifacts.sh
@@ -1,35 +1,37 @@
 #! /bin/bash
 
-echo "Installing dependencies"
-echo "_______________________________"
-
-npm i -g npm serverless
-
-echo "Deploying to AWS environment"
-echo "_______________________________"
-
 ROOT_DIR=$(pwd)
 ARTIFACT_DIR=dist/artifact/
 DOCS_PATH=dist/docs/api/openapi.yml
 
-for dir in $CODEBUILD_SRC_DIR/src/apps/*/*/$ARTIFACT_DIR/*
-do
+if [ -z "$CODEBUILD_SRC_DIR" ]; then
+    CODEBUILD_SRC_DIR=$ROOT_DIR
+else
+    echo "Installing dependencies"
+    echo "_______________________________"
+
+    npm i -g npm serverless
+fi
+
+echo "Deploying to AWS environment"
+echo "_______________________________"
+
+for dir in $CODEBUILD_SRC_DIR/src/apps/*/*/$ARTIFACT_DIR/*/
+do  
     ENV=$(basename $dir)
-    PACKAGE_DIR=$dir
-    SERVERLESS_DIR=$dir/serverless
     cd $dir/../../../
     SERVICE_FLAVOR=$(basename $(pwd))
     DOCS=$(pwd)/$DOCS_PATH
 
+    cp -r $dir/serverless/. ./
+
+    # .serverless folder must be used to prevent current serverless issues --> https://github.com/serverless/serverless/issues/5381
+    mkdir -p .serverless
+    cp -r $dir/. .serverless
+
     source .env.$ENV
 
     echo "Deploying <$PROJECT ($SERVICE_FLAVOR)> into <$ENV> environment"
-
-    cp -r $SERVERLESS_DIR/* .
-    rm -rf $SERVERLESS_DIR
-    # .serverless folder must be used to prevent current serverless issues --> https://github.com/serverless/serverless/issues/5381
-    mkdir -p .serverless
-    cp -r $PACKAGE_DIR .serverless
 
     sls deploy --stage $ENV --package .serverless
 
