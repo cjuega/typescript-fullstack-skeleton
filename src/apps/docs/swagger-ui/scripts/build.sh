2b631282e2490a583a6855a4efe7e26d402c08d7
Carlos Juega <juancarlos.juega@optivamedia.com>
2022-04-06 11:52:03 +0200

    refactor: scripts/build simplified

--- src/apps/docs/swagger-ui/scripts/build.sh
+++ src/apps/docs/swagger-ui/scripts/build.sh
@@ -19,15 +19,13 @@ mkdir .tmp
 rm -rf dist/
 mkdir -p dist/apis
 
-cd .tmp
-
 echo '---- Fetching SwaggerUI $SWAGGER_UI_VERSION -->'
-wget https://github.com/swagger-api/swagger-ui/archive/v$SWAGGER_UI_VERSION.tar.gz
-tar -zxvf v$SWAGGER_UI_VERSION.tar.gz
-rm v$SWAGGER_UI_VERSION.tar.gz
-cp -r swagger-ui-$SWAGGER_UI_VERSION/dist/ ../dist/
-cp ../src/index.html ../dist/
-cp ../src/docs-list.json ../dist/
+wget https://github.com/swagger-api/swagger-ui/archive/v$SWAGGER_UI_VERSION.tar.gz -P .tmp/
+tar -zxvf .tmp/v$SWAGGER_UI_VERSION.tar.gz -C .tmp/
+rm .tmp/v$SWAGGER_UI_VERSION.tar.gz
+cp -r .tmp/swagger-ui-$SWAGGER_UI_VERSION/dist/ dist/
+cp src/index.html dist/
+cp src/docs-list.json dist/
 
 if [ -z "$DOCS_S3_BUCKET" ]; then
     echo "DOCS_S3_BUCKET not found in <.env.$ENV>. I can't try to update docs-list.json from an existing deployment."
@@ -45,5 +43,5 @@ if [ ! -z "$AWS_PROFILE" ]; then
 fi
 
 echo '---- Retrieve exising API swagger files -->'
-aws s3 cp s3://$DOCS_S3_BUCKET/docs-list.json ../dist/docs-list.json $AWS_ARGS
-aws s3 sync s3://$DOCS_S3_BUCKET/apis/ ../dist/apis/ $AWS_ARGS
+aws s3 cp s3://$DOCS_S3_BUCKET/docs-list.json dist/docs-list.json $AWS_ARGS
+aws s3 sync s3://$DOCS_S3_BUCKET/apis/ dist/apis/ $AWS_ARGS
