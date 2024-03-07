26f7028f2c615cf7e3da24544327bf8f1347e58f
Carlos Juega <carlosjuegasi@gmail.com>
2024-03-07 21:29:57 +0100

    feat(@context/shared): rabbitMQ configurer to setup exchanges, queues, etc

---
+++ src/contexts/shared/src/infrastructure/eventBus/rabbitmq/rabbitmqConfigurer.ts
@@ -0,0 +1,66 @@
+import DomainEvent from '@src/domain/eventBus/domainEvent';
+import { DomainEventSubscriber } from '@src/domain/eventBus/domainEventSubscriber';
+import RabbitmqConfig from '@src/infrastructure/eventBus/rabbitmq/rabbitmqConfig';
+import RabbitmqConnection from '@src/infrastructure/eventBus/rabbitmq/rabbitmqConnection';
+
+interface QueueBindingKeys {
+    queueName: string;
+    bindingKeys: string[];
+}
+
+export interface RabbitmqConfigurerOptions {
+    retryDelay: number;
+}
+
+export default class RabbitmqConfigurer {
+    private readonly connection: RabbitmqConnection;
+
+    private readonly queues: Array<QueueBindingKeys>;
+
+    private readonly config: RabbitmqConfig;
+
+    private readonly options: RabbitmqConfigurerOptions;
+
+    constructor(
+        connection: RabbitmqConnection,
+        subscribers: DomainEventSubscriber<DomainEvent>[],
+        config: RabbitmqConfig,
+        options: RabbitmqConfigurerOptions
+    ) {
+        this.connection = connection;
+        this.queues = subscribers.map((s) => ({ queueName: s.name(), bindingKeys: s.subscribedTo().map(({ eventName }) => eventName) }));
+        this.config = config;
+        this.options = options;
+    }
+
+    async configure(): Promise<void> {
+        await this.connection.connect();
+
+        await this.connection.setupExchange(this.config.exchange);
+        await this.connection.setupExchange(RabbitmqConnection.retryName(this.config.exchange));
+        await this.connection.setupExchange(RabbitmqConnection.deadLetterName(this.config.exchange));
+
+        await Promise.all(this.queues.map((q) => this.setupQueue(q)));
+
+        await this.connection.close();
+    }
+
+    private async setupQueue(queue: QueueBindingKeys): Promise<void> {
+        await this.connection.setupQueue(this.config.exchange, queue.queueName, [...queue.bindingKeys, queue.queueName]);
+
+        await this.connection.setupQueue(
+            RabbitmqConnection.retryName(this.config.exchange),
+            RabbitmqConnection.retryName(queue.queueName),
+            [queue.queueName],
+            this.config.exchange,
+            queue.queueName,
+            this.options.retryDelay
+        );
+
+        await this.connection.setupQueue(
+            RabbitmqConnection.deadLetterName(this.config.exchange),
+            RabbitmqConnection.deadLetterName(queue.queueName),
+            [queue.queueName]
+        );
+    }
+}
--- src/contexts/shared/src/infrastructure/eventBus/rabbitmq/rabbitmqConnection.ts
+++ src/contexts/shared/src/infrastructure/eventBus/rabbitmq/rabbitmqConnection.ts
@@ -1,16 +1,60 @@
 /* eslint-disable no-underscore-dangle */
 import RabbitmqConfig from '@src/infrastructure/eventBus/rabbitmq/rabbitmqConfig';
-import { ConfirmChannel, Connection, connect as rabbitConnect } from 'amqplib';
+import {
+    ConfirmChannel, Connection, ConsumeMessage, MessagePropertyHeaders, connect as rabbitConnect
+} from 'amqplib';
 
 export default class RabbitmqConnection {
+    private static readonly RETRY_SUFFIX = '.retry';
+
+    private static readonly DEADLETTER_SUFFIX = '.deadletter';
+
     private readonly config: RabbitmqConfig;
 
     private _connection?: Connection;
 
     private _channel?: ConfirmChannel;
 
+    static retryName(name: string): string {
+        return `${name}${RabbitmqConnection.RETRY_SUFFIX}`;
+    }
+
+    static deadLetterName(name: string): string {
+        return `${name}${RabbitmqConnection.DEADLETTER_SUFFIX}`;
+    }
+
     constructor(config: RabbitmqConfig) {
         this.config = config;
+    }
+
+    async setupExchange(exchangeName: string): Promise<void> {
+        await this.channel.assertExchange(exchangeName, 'topic', { durable: true });
+    }
+
+    async setupQueue(
+        exchangeName: string,
+        queueName: string,
+        bindingKeys: string[],
+        deadletterExchange?: string,
+        deadletterQueue?: string,
+        msgTtl?: number
+    ): Promise<void> {
+        await this.channel.assertQueue(queueName, {
+            exclusive: false,
+            durable: true,
+            autoDelete: false,
+            arguments: RabbitmqConnection.generateQueueArgs(deadletterExchange, deadletterQueue, msgTtl)
+        });
+
+        await Promise.all(bindingKeys.map((bindingKey) => this.channel.bindQueue(queueName, exchangeName, bindingKey)));
+    }
+
+    private static generateQueueArgs(deadletterExchange?: string, deadletterQueue?: string, msgTtl?: number): Record<string, unknown> {
+        return {
+            ...(deadletterExchange && { 'x-dead-letter-exchange': deadletterExchange }),
+            ...(deadletterQueue && { 'x-dead-letter-routing-key': deadletterQueue }),
+            ...(msgTtl && { 'x-message-ttl': msgTtl })
+        };
     }
 
     private get connection(): Connection {
